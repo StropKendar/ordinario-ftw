75602c6ed8343a74c474f18e0db44a227060a9c9
StropKendar <stropkendar@gmail.com>
2026-06-07 20:28:10 -0600

    Creación de catalogo y funciones js

---
+++ catalogo.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", cargarCatalogo);
+
+function cargarCatalogo() {
+
+    fetch("productos.xml")
+        .then(response => response.text())
+        .then(data => {
+
+            const parser = new DOMParser();
+            const xml = parser.parseFromString(data, "text/xml");
+
+            const laptops = xml.getElementsByTagName("laptop");
+
+            const contenedor = document.getElementById("contenedor-productos");
+
+            contenedor.innerHTML = "";
+
+            for (let laptop of laptops) {
+
+                const id = laptop.getElementsByTagName("id")[0].textContent;
+                const nombre = laptop.getElementsByTagName("nombre")[0].textContent;
+                const procesador = laptop.getElementsByTagName("procesador")[0].textContent;
+                const ram = laptop.getElementsByTagName("ram")[0].textContent;
+                const almacenamiento = laptop.getElementsByTagName("almacenamiento")[0].textContent;
+                const precio = laptop.getElementsByTagName("precio")[0].textContent;
+
+                contenedor.innerHTML += `
+        <div class="product-card">
+
+            <img src="imagenes/imagen-default.avif" alt="${nombre}">
+
+            <h3>${nombre}</h3>
+
+            <p>${procesador}</p>
+
+            <p>${ram} | ${almacenamiento}</p>
+
+            <p class="price">$${precio}</p>
+
+            <button onclick="verDetalles(${id})">
+                Ver detalles
+            </button>
+
+        </div>
+    `;
+            }
+
+        })
+        .catch(error => {
+            console.error("Error al cargar el XML:", error);
+        });
+
+}
+
+
+function verDetalles(id) {
+
+    window.location.href = `detalle.html?id=${id}`;
+
+}
